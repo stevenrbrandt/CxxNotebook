ffe7da2d11caf119331ab435821e110d82e8ac9b
Steven R. Brandt <sbrandt@cct.lsu.edu>
2026-03-16 16:31:59 -0500

    Tweak startup

--- notebk.sh
+++ notebk.sh
@@ -1,3 +1,5 @@
+PORT=8004
+TOKEN=
 echo "Starting notebook"
-echo jupyter notebook --allow-root --ip=0.0.0.0 --port=${PORT} --no-browser --NotebookApp.token=""
-jupyter notebook --allow-root --ip=0.0.0.0 --port=${PORT} --no-browser --NotebookApp.token=""
+echo jupyter notebook --ip=0.0.0.0 --port=${PORT} --no-browser --NotebookApp.token="${TOKEN}"
+jupyter notebook --ip=0.0.0.0 --port=${PORT} --no-browser --NotebookApp.token="${TOKEN}"
